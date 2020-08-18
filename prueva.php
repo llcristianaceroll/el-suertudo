51ad5fdd71ddc569c6d4b0ea59af24512402f0ca
camilo7896 <63164154+camilo7896@users.noreply.github.com>
2020-08-18 16:21:14 -0500

    Update prueva.php

--- prueva.php
+++ prueva.php
@@ -1,15 +1,4 @@
-<?php
-   
-$servername = "localhost";
-$username = "juego";
-$password = "";
-$database = "datos_del_jugador";
 
-$enlace = mysqli_connect($servername, $username, $password, $database);
-
-if(!$enlace){
-    echo "error en laconexion con el servidor";
-}?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -27,58 +16,14 @@ if(!$enlace){
                 <!--***************** Encabezado ( titulo ) **********************************************-->
 
 <header class="titulo">
-                    <h1> <div class="spam"> l  f q k o </div> El suertudo <div class="spam"> m n a f  l </div> </h1>
+<a href="sorteo.php">    <img  src="img/logoheade.jpg" alt="">  </a>
                        
             </header>
                 <!--***************** Fin de Encabezado ( titulo ) **********************************************-->
 
  <section>
         <div>
-        <?php
-        if(isset($_POST['registrarse'])){
-            $nombre=$_POST['nombre'];
-            $apellido=$_POST['apellido'];
-            $stelefono=$_POST['telefono'];
-            $dato=$_POST['dato'];
-            
-            
-            $insertarDatos="INSERT INTO datos_de_inscritos VALUES(
-                                            '$nombre',
-                                            
-                                            '$apellido',
-                                            
-                                            '$telefono',
-
-                                            '$dato'
-                                            
-                                           /*  '$correo',
-                                            
-                                           '$fecha'*/
-                                            )";
-                                            
-        $ejecutarinsertar=mysqli_query($enlace, $insertarDatos);
-        echo "<h2>informacion recibida<h2>" . "<hr/>";
-        
-        echo "su nombre es :" . $nombre . "<hr/>";
-        
-        echo "su apellido es :" . " " .$apellido . "<hr/>";
-        
-        echo "su telefono es :" . " " . $telefono . "<hr/>";
-        
-        echo "su numero es :" . " " . $dato . "<hr/>";
-        
-            //verificamos la ejecucion
-            
-            if(!$ejecutarinsertar){
-                echo "hay un error en la linea sql";
-            }
-             else{
-                echo"Datos Guardados Correctamente<br><a href='tiquet.php'>Volver</a>";
-            }
-        
-          }
-             mysqli_close($enlace);
-     ?>
+       
         </div>              
             <hr>
                 <a href="tiquet.php"> <input type="submit" value="volver"></a>
